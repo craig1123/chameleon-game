1cda63bd46deeb390b4eeda8114924d7d391899d
Craig Walker <craigwalker1123@gmail.com>
2023-01-07 15:09:16 -0800

    Update config.js

--- consts/config.js
+++ consts/config.js
@@ -3,7 +3,7 @@ const prod = process.env.NODE_ENV === 'production';
 const config = {
   development: !prod,
   ga: prod ? 'G-PKG6DHM8QM' : '',
-  url: prod ? 'https://the-chameleon.onrender.com/' : 'http://localhost:3000',
+  url: prod ? 'https://the-chameleon.onrender.com' : 'http://localhost:3000',
 };
 
 export default config;
